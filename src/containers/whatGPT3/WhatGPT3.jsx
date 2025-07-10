284503d73c470397120d283e4d36199404014402
Christian <Christianmnaira@gmail.com>
2025-07-10 14:30:10 +0800

    feat: initialized the contents of WhatGPT3 component

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -3,8 +3,17 @@ import "./WhatGPT3.css";
 
 const WhatGPT3 = () => {
   return (
-    <div>
-      WhatGPT3
+    <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
+      <div className="gpt3__whatgpt3-feature">
+
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
+        <p>Explore the Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+
+      </div>
     </div>
   )
 }
